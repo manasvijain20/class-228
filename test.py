b4a60163bc459c9476db8ceccaa377cb4974d5cb
manasvijain20 <74360258+manasvijain20@users.noreply.github.com>
2023-09-12 18:49:32 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+for i in range(11,21):
+    if i % 2 == 0:
+        square = i*i
+        # print(square)
+    # square = i*i
+    # print(square)
+
+# squared_values = [i for i in range(10,21) if i%2 == 0]
+# print(squared_values)
+
+continets = ["asia", "oceania", "north america"]
+
+oceans = ("Indian ocean", "Pacific ocean", "Arctic")
+
+# continets[1]= "Aus"
+# print(continets)
+
+oceans[1] = "Atlantic"
+print(oceans)
